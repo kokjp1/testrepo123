1c8094272ef6c0350f6232b8bb3d29aa693b02b6
kokjp1 <james.kok@hva.nl>
2025-02-13 12:57:22 +0100

    express install

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const goofyahh = 123
+const express = require('express'); 
+const app = express();
 
+// [EXPRESS] intialization 
+
+express().get('/', onhome)
+express().listen(5500)
+console.log("✅ De server runnt op port 8000.✅")
+
+// --
+
+function onhome(req, res) {
+    res.send('<h1>Hello World</h1>');
+}
